672cb17b55c9a21749f9993b9fea47cdaac96f7f
Ray Joshua Balingkit <rayjoshua432@gmail.com>
2022-06-19 16:08:47 +0800

    React JS Basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// React - CamelCase
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react'
+import "./style/style.css"
+
+
+const App = () => {
+    // Variables
+    const name = 'Ray Joshua'
+
+    // States
+    const [count, setCount] = useState(0)
+    const [input, setInput] = useState('')
+
+    // Function for incrementing the count
+    const incrementCount = () => {
+        setCount(count + 1)
+    }
+
+    // Function for decrementing the count
+    const decrementCount = () => {
+        setCount(count - 1)
+    }
+
+    // Function for on change event
+    const onChange = (element) => {
+        setInput(element.target.value)
+    }
+
+    // Display component
+
+
+    return ( 
+        // <React.fragment></React.fragment> or <></> or 1 parent element
+        <div className='container'>
+            <div className='msg'>Hello, {name}.</div>
+            <p>Count: {count}</p>
+            <button onClick={incrementCount}>Increment</button>
+            <button onClick={decrementCount}>Decrement</button>
+            <br/>
+            <br/>
+            <p>On Change:</p>
+            <input type="text" onChange={onChange} value={input}/>
+            <br/>
+            <br/>
+            <p>Display Component:</p>
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
